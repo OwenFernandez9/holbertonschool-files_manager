256cde37b68271fc89e087ba8fcf1f008445ccf0
OwenFernandez9 <9357@holbertonstudents.com>
2025-09-02 11:37:38 -0300

    redis

---
+++ utils/redis.mjs
@@ -0,0 +1,34 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+      console.error('Redis Client Error:', err);
+    });
+
+    this.client.connect().catch((err) => {
+      console.error('Connection failed:', err);
+    });
+  }
+
+  isAlive() {
+    return this.client && this.client.isOpen;
+  }
+
+  async get(key) {
+    return this.client.get(key);
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value, { EX: duration });
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
